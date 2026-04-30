b9bec7a5d641a6832a20cdbcc02fdf981c867c42
GENRYMUSICO <GENRYMUSICOOF@GMAIL.COM>
2026-04-30 17:09:29 -0500

    Update guardian.py

--- guardian.py
+++ guardian.py
@@ -1,1 +1,23 @@
+import time
+import os
+import subprocess
+import sys
 
+# Nombre del archivo principal del antivirus
+ANTIVIRUS_SCRIPT = "antivirus_genry.py"
+
+def vigilar_tanque():
+    while True:
+        # Verificamos si el proceso del antivirus está corriendo
+        # Si no lo encuentra, lo vuelve a lanzar en silencio
+        salida = subprocess.check_output('tasklist', shell=True).decode()
+        if "python" in salida.lower() and ANTIVIRUS_SCRIPT.lower() in salida.lower():
+            pass
+        else:
+            # Si el tanque cayó, lo resucitamos
+            subprocess.Popen([sys.executable, ANTIVIRUS_SCRIPT])
+        
+        time.sleep(2) # Verifica cada 2 segundos
+
+if __name__ == "__main__":
+    vigilar_tanque()
