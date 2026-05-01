b7503b03c4832be0bee9cf47f0c844033dde53d3
GENRYMUSICO <GENRYMUSICOOF@GMAIL.COM>
2026-05-01 12:15:48 -0500

    Update guardian.py

--- guardian.py
+++ guardian.py
@@ -8,16 +8,19 @@ ANTIVIRUS_SCRIPT = "antivirus_genry.py"
 
 def vigilar_tanque():
     while True:
-        # Verificamos si el proceso del antivirus está corriendo
-        # Si no lo encuentra, lo vuelve a lanzar en silencio
-        salida = subprocess.check_output('tasklist', shell=True).decode()
-        if "python" in salida.lower() and ANTIVIRUS_SCRIPT.lower() in salida.lower():
-            pass
-        else:
-            # Si el tanque cayó, lo resucitamos
-            subprocess.Popen([sys.executable, ANTIVIRUS_SCRIPT])
+        try:
+            # Verificamos si el proceso del antivirus está corriendo
+            salida = subprocess.check_output('tasklist', shell=True).decode()
+            if "python" in salida.lower() and ANTIVIRUS_SCRIPT.lower() in salida.lower():
+                pass
+            else:
+                # Si el tanque cayó, lo resucitamos en silencio
+                subprocess.Popen([sys.executable, ANTIVIRUS_SCRIPT], 
+                                 creationflags=subprocess.CREATE_NO_WINDOW)
+        except Exception as e:
+            print(f"Error en vigilancia: {e}")
         
-        time.sleep(2) # Verifica cada 2 segundos
+        time.sleep(2) # El pulso del Búnker
 
 if __name__ == "__main__":
     vigilar_tanque()
